9b1029a5730df7576e5816cf6a0bee3c3775e82b
clementine mirande <clementine.mirande@gmail.com>
2025-02-02 19:11:50 +0100

    add record for tennis.py

--- S2/RL/TP1/cart_pole.py
+++ S2/RL/TP1/cart_pole.py
@@ -1,23 +1,19 @@
 import gymnasium as gym
 
-# Création de l'environnement CartPole
 env = gym.make('CartPole-v1', render_mode="human")
 
-# Afficher l'espace d'état
 print("\nEspace d'état:")
 print(env.observation_space)
 print("Valeurs maximales:", env.observation_space.high)
 print("Valeurs minimales:", env.observation_space.low)
 
-# Afficher l'espace d'actions
+
 print("\nEspace d'actions:")
 print(env.action_space)
 
-# Réinitialiser l'environnement et afficher l'état initial
-initial_state, _ = env.reset()  # La nouvelle version retourne (state, info)
+initial_state, _ = env.reset() 
 print("\nÉtat initial:", initial_state)
 
-# 3.2 Équilibrage avec politique aléatoire
 num_episodes = 100
 num_timesteps = 50
 
@@ -28,10 +24,8 @@ for episode in range(num_episodes):
     total_reward = 0
     
     for t in range(num_timesteps):
-        # Sélectionner une action aléatoire
         action = env.action_space.sample()
         
-        # Effectuer l'action aléatoire
         state, reward, terminated, truncated, _ = env.step(action)
         total_reward += reward
         
--- S2/RL/TP1/tennis.py
+++ S2/RL/TP1/tennis.py
@@ -1,26 +1,29 @@
 import gymnasium as gym
+from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
 import ale_py
-import time 
 
 gym.register_envs(ale_py)
 
-env = gym.make('ALE/Tennis-v5', render_mode="human", obs_type="rgb")
-obs, info = env.reset()
+num_episodes  = 100
+num_timesteps = 50
 
-episode = 0
-total_reward = 0
+env = gym.make('ALE/Tennis-v5', render_mode="rgb_array", obs_type="rgb")
 
-for _ in range(1000):
-    action = env.action_space.sample()
-    obs, reward, terminated, truncated, info = env.step(action)
-    total_reward += reward
-    
-    time.sleep(0.02)
-    
-    if terminated or truncated:
-        print(f"Episode {episode} terminé avec score: {total_reward}")
-        episode += 1
-        total_reward = 0
-        obs, info = env.reset()
+env = RecordVideo(env, video_folder="./recordings", name_prefix="eval", episode_trigger=lambda x: True)
+env = RecordEpisodeStatistics(env, buffer_length=num_episodes)
+
+for episode_num in range(num_episodes):
+    obs, info = env.reset()
+
+    episode_over = False
+    while not episode_over:
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+
+        episode_over = terminated or truncated
 
 env.close()
+
+print(f'Episode time taken: {env.time_queue}')
+print(f'Episode total rewards: {env.return_queue}')
+print(f'Episode lengths: {env.length_queue}')
